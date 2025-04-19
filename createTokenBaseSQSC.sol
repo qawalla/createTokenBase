b27978deed7063e76c096940b37701e0138b1b3e
Bevanz <alex.bevans@gmail.com>
2025-04-18 22:28:17 -0700

    Update createTokenBaseSQSC.sol
    updated contract

--- createTokenBaseSQSC.sol
+++ createTokenBaseSQSC.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract TokenFactory {
+contract createTokenBaseSQSC {
     address[] public deployedTokens;
 
     event TokenCreated(address tokenAddress, string name, string symbol, uint256 totalSupply, uint8 decimals, address owner);
